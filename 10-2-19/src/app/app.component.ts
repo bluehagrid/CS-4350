f816fc49ff2d270b383c462578dc955f9633065f
Bluehagrid <jacob.jackson@dixiesuccess.org>
2019-10-04 14:49:51 -0600

    Added message components for chatroom

--- 10-2-19/src/app/app.component.ts
+++ 10-2-19/src/app/app.component.ts
@@ -7,6 +7,50 @@ import { ISocketMessage } from './interfaces';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public message: string;
+  public allMessages: ISocketMessage[] = [];
   private username: string;
   private io: {emit: (key: string, value: string) => void, on: (event: string, callback: (data: ISocketMessage) => void) => void};
+  private isConnected = false;
+
+  constructor(){
+    
+  }
+
+  public addParticipantsMessage(data: ISocketMessage) {
+
+  }
+
+  public sendMessage() {
+
+  }
+
+  public onKeyDown(event: KeyboardEvent) {
+
+  }
+
+  public log(message: string, shouldPrepend?: boolean) {
+
+  }
+
+  public addChatMessage(data: ISocketMessage) {
+
+  }
+
+  public scrollToMessageTop() {
+
+  }
+
+  public getUsernameColor(username: string) {
+
+  }
+
+  public inputMessageClick(event: MouseEvent) {
+
+  }
+
+  private setUpListeners() {
+
+  }
+
 }
--- 10-2-19/src/app/app.module.ts
+++ 10-2-19/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
